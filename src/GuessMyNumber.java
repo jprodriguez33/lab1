740dd1610110a7da3205bc7ac4a8e33525262169
jprodriguez33 <johnpaul334@icloud.com>
2023-01-22 14:38:08 -0700

    Fixes

--- src/GuessMyNumber.java
+++ src/GuessMyNumber.java
@@ -1,4 +1,4 @@
-import java.util.Arrays;
+
 import java.util.Scanner;
 import java.util.stream.IntStream;
 
@@ -7,45 +7,40 @@ public class GuessMyNumber {
         Scanner scan = new Scanner(System.in);
         String response;
 
-
-        do{
-
             int mid = (first+Subn)/2;
 
             System.out.println("INSTRUCTIONS: Please enter C for correct, H for too high, or L for too low.");
             System.out.println(">Is your number "+ mid +"?<");
             System.out.println("Enter your response (H/L/C) :");
-            response = scan.nextLine();
 
-            if (response.equals("L")){
+            response = scan.nextLine();
+        do {
+
+
+            if (response.equals("L")) {
                 first = mid + 1;
             }
-            if (response.equals("H")){
-                Subn = mid -1;
+            if (response.equals("H")) {
+                Subn = mid - 1;
             }
-            if (response.equals("C")){
-                System.out.println("Your number is "+mid+"!");
-                break;
+            if (response.equals("C")) {
+                System.out.println("Your number is " + mid + "!");
+
             }
-
-
         }while(response != "C");
 
         return -1;
     }
     public static void main(String[] args){
-
         Scanner scan = new Scanner(System.in);
-        int n=1;
+
+        int n;
         int Subn;
-        int first = 0;
-
-
 
             System.out.println("Please input a number: ");
             n = scan.nextInt();
-        if (n>0) {
 
+        if (n>0) {
 
             int[] array = IntStream.rangeClosed(0, n).toArray();
 
@@ -57,8 +52,10 @@ public class GuessMyNumber {
             int result = BinarySearch(array, 0, Subn);
         }
         else {
+
             System.out.println("Please enter a positive number: ");
             n = scan.nextInt();
+
             int[] array = IntStream.rangeClosed(0, n).toArray();
 
             Subn = n - 1;
@@ -70,5 +67,5 @@ public class GuessMyNumber {
 
         }
 
-        }
+    }
 }
