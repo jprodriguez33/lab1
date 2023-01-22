f918030869b289c8ef8ff39bb9d17024839a201b
jprodriguez33 <johnpaul334@icloud.com>
2023-01-21 20:22:58 -0700

    Y

--- src/PersonalizedHelloWorld.java
+++ src/PersonalizedHelloWorld.java
@@ -1,11 +1,15 @@
 import java.util.Scanner;
 public class PersonalizedHelloWorld {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
 
-    Scanner scan = new Scanner(System.in);
-        System.out.println("What is your name?");
+        String name;
+        do {
+            System.out.println("What is your name?");
+            name = scan.nextLine();
 
-    String name = scan.nextLine();
-        System.out.println("Hello, "+name+"!");
+
+        } while (name.isEmpty());
+        System.out.println("Hello, " + name + "!");
     }
 }
