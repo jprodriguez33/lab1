ece3ced83c130976a1036cb56e0429512db767cc
jprodriguez33 <johnpaul334@icloud.com>
2023-01-21 20:27:50 -0700

    Fixes

--- src/PersonalizedHelloWorld.java
+++ src/PersonalizedHelloWorld.java
@@ -2,14 +2,14 @@ import java.util.Scanner;
 public class PersonalizedHelloWorld {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
         String name;
+
         do {
             System.out.println("What is your name?");
-            name = scan.nextLine();
-
+                name = scan.nextLine();
 
         } while (name.isEmpty());
+
         System.out.println("Hello, " + name + "!");
     }
 }
